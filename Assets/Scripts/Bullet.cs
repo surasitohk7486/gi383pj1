2d9d048218357467720979a9f4986e5682382391
Nangil <surasit.dang@bumail.net>
2025-01-28 19:42:33 +0700

    Pz1Pz2

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Animator objectAnimator;  // Reference to the Animator of the object being hit
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // เช็คว่าโดน GameObject ที่เราต้องการหรือไม่ (คุณสามารถเปลี่ยนชื่อหรือชนิดของวัตถุได้ตามที่ต้องการ)
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            // เรียกใช้ Bool ใน Animator ของ Object
+            if (objectAnimator != null)
+            {
+                objectAnimator.SetBool("Pz1Sc", true);  // ตั้งค่า Bool "PzSc" เป็น true เพื่อเริ่มเล่นแอนิเมชัน
+            }
+
+            // ทำลายกระสุน
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Pz2Gravity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pz2Gravity : MonoBehaviour
+{
+    public Animator Pz1;  // Reference to the Animator of the object being hit
+    public Animator Pz21;
+    public Animator Pz22;
+    public Animator Pz3;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // เช็คว่าโดน GameObject ที่เราต้องการหรือไม่ (คุณสามารถเปลี่ยนชื่อหรือชนิดของวัตถุได้ตามที่ต้องการ)
+        if (collision.gameObject.CompareTag("Pz2"))
+        {
+            // เรียกใช้ Bool ใน Animator ของ Object
+            if (Pz1 != null)
+            {
+                Pz1.SetBool("hit", true);  // ตั้งค่า Bool "PzSc" เป็น true เพื่อเริ่มเล่นแอนิเมชัน
+                Pz21.SetBool("hit", true);
+                Pz22.SetBool("hit", true);
+                Pz3.SetBool("hit", true);
+            }
+
+            // ทำลายกระสุน
+            Destroy(gameObject);
+        }
+    }
+}
