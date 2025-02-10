52e1c0af7df95c7862edba035be05b7a6e6a8dee
Nangil <surasit.dang@bumail.net>
2025-02-10 18:59:03 +0700

    Platforms Spin

---
+++ Assets/Scripts/PlayerFollowPlatforms.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollowPlatforms : MonoBehaviour
+{
+    private bool isOnPlatform = false;
+    private Transform platformTransform;
+    private Vector3 lastPlatformPosition;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            isOnPlatform = true;
+            platformTransform = collision.transform;
+            lastPlatformPosition = platformTransform.position;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            isOnPlatform = false;
+            platformTransform = null;
+        }
+    }
+
+    void Update()
+    {
+        if (isOnPlatform && platformTransform != null)
+        {
+            // คำนวณการเคลื่อนที่ของแพลตฟอร์ม
+            Vector3 platformMovement = platformTransform.position - lastPlatformPosition;
+
+            // ให้ Player ติดตามแพลตฟอร์ม
+            transform.position += platformMovement;
+
+            // อัปเดตตำแหน่งล่าสุดของแพลตฟอร์ม
+            lastPlatformPosition = platformTransform.position;
+        }
+    }
+}
