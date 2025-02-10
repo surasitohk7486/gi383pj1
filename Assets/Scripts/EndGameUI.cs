6d2a1c38a4c22bfb15549d5cb176a0972976ca46
Nangil <surasit.dang@bumail.net>
2025-02-10 22:09:17 +0700

    EndGame

---
+++ Assets/Scripts/EndGameUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameUI : MonoBehaviour
+{
+    [SerializeField] private GameObject live;
+    [SerializeField] private Button myButton; // ใช้ SerializeField ให้แสดงใน Inspector
+    [SerializeField] private Button myButton2;
+
+    void Start()
+    {
+        if (myButton != null)
+        {
+            myButton.onClick.AddListener(OnClickLive);
+        }
+
+        if (myButton2 != null)
+        {
+            myButton.onClick.AddListener(OnClickLeave);
+        }
+
+    }
+    void OnClickLive()
+    {
+        live.SetActive(true);
+    }
+
+    void OnClickLeave()
+    {
+        
+    }
+}
